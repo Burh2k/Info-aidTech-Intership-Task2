d272327db0366543e13f51a3066b3080f069611f
Muhammad Burhan Ahmed <92679045+Burh2k@users.noreply.github.com>
2023-07-07 16:03:38 +0500

    Add files via upload

--- ScriptFile.js
+++ ScriptFile.js
@@ -1,8 +1,23 @@
 function show(parameter) {
     var nums = parameter.getAttribute("data-value");
     var numInputs = document.getElementsByClassName("input");
-
-    for (var i = 0; i < numInputs.length; i++) {
-        numInputs[i].value = numInputs[i].value + nums;
+    if (nums === "") {
+        for (var i = 0; i < numInputs.length; i++) {
+            numInputs[i].value = "";
+        }
     }
+    else {
+        for (var i = 0; i < numInputs.length; i++) {
+            numInputs[i].value = numInputs[i].value + nums;
+        }
+    }
+}
+function backspace() {
+    var output = document.getElementsByClassName("input")[0].value;
+    output = output.slice(0, -1);
+    document.getElementsByClassName("input")[0].value = output;
+}
+function result() {
+    var output = document.getElementsByClassName("input").value;
+
 }
