00e19ecf93a993749e7ead4518521639dbe7ad32
Muhammad Burhan Ahmed <92679045+Burh2k@users.noreply.github.com>
2023-07-10 17:19:04 +0500

    Add files via upload

--- ScriptFile.js
+++ ScriptFile.js
@@ -1,3 +1,18 @@
+tailwind.config = {
+    content: [],
+    theme: {
+        screens: {
+            'sm': '400px', //If you only add one screen size in js fiel, it will override all the screens availavle
+            'lg': '1024px',
+        },
+        extend: {
+
+        },
+    },
+    plugins: [],
+}
+
+
 function show(parameter) {
     var nums = parameter.getAttribute("data-value");
     var numInputs = document.getElementsByClassName("input");
@@ -18,6 +33,32 @@ function backspace() {
     document.getElementsByClassName("input")[0].value = output;
 }
 function result() {
-    var output = document.getElementsByClassName("input").value;
+    var output = document.getElementsByClassName("input")[0].value;
+    document.getElementsByClassName("input")[0].value = eval(output);
+}
+document.getElementById("theme").textContent = "DARK";
+function change() {
+    var bg = document.getElementById("theme");
 
+    var body = document.getElementById("body");
+    var navbar = document.getElementById("navbar");
+    var header = document.getElementById("heading");
+    var calcu_boundary = document.getElementById("cal");
+
+    if (bg.textContent === "DARK") {
+        bg.textContent = "LIGHT";
+        bg.style.color = "black";
+        body.style.background = "black";
+        navbar.style.background = "white";
+        header.style.color = "black";
+        calcu_boundary.style.borderColor="white";
+    }
+    else {
+        bg.textContent = "DARK";
+        bg.style.color = "white";
+        body.style.background = "#D1D5DB";
+        navbar.style.background = "#10161D";
+        header.style.color = "white";
+        calcu_boundary.style.borderColor="black";
+    }
 }
